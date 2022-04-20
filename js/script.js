caf16fa78fe3a1ab0c56addc2fb27d63ae1afe64
ManuelBenini <manuelbenini1905@gmail.com>
2022-04-20 14:49:08 +0200

    Primo commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*
+  CONSEGNA - Palidroma
+  1a - Chiedere all’utente di inserire una parola
+  2a - Creare una funzione per capire se la parola inserita è palindroma
+  Bonus: L’inserimento avviene tramite un campo input
+
+  CONSEGNA 2 - Pari e Dispari
+  1b - L’utente sceglie pari o dispari
+  2b - inserisce un numero da 1 a 5.
+  3b - Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+  4b - Sommiamo i due numeri
+  5b - Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+  6b -Dichiariamo chi ha vinto.
+  Bonus: L’inserimento avviene tramite un campo input
+*/
